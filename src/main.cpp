ffee43f6f0fe63b00265b4a254a2ef46e4753108
Ada <adayke10@gmail.com>
2025-04-02 01:29:52 -0700

    led for 3 way switch works but yellow looks nasty

--- src/main.cpp
+++ src/main.cpp
@@ -3,7 +3,12 @@
 // Define the GPIO pins for the switches
 #define leftSwitchPin 3   // Connect left switch here
 #define rightSwitchPin 4  // Connect right switch here
-#define ledPin 8
+#define ledPin 8          // Existing single-color LED
+
+// Define the GPIO pins for the RGB LED
+#define rPin 2    // Connect red pin of RGB LED here
+#define gPin 1   // Connect green pin of RGB LED here
+#define bPin 0   // Connect blue pin of RGB LED here
 
 // Enumeration for the switch modes
 enum SwitchMode {
@@ -20,7 +25,12 @@ void setup() {
   // Configure the switch pins as inputs with internal pull-ups enabled
   pinMode(leftSwitchPin, INPUT_PULLUP);
   pinMode(rightSwitchPin, INPUT_PULLUP);
+  
+  // Configure the LED pins as outputs
   pinMode(ledPin, OUTPUT);
+  pinMode(rPin, OUTPUT);
+  pinMode(gPin, OUTPUT);
+  pinMode(bPin, OUTPUT);
 }
 
 void loop() {
@@ -37,7 +47,7 @@ void loop() {
     currentMode = MODE_NEITHER;
   }
   
-  // Print the current mode to the Serial Monitor
+  // Control the single LED and print the current mode to the Serial Monitor
   switch (currentMode) {
     case MODE_LEFT:
       digitalWrite(ledPin, HIGH);
@@ -49,40 +59,32 @@ void loop() {
       break;
     case MODE_RIGHT:
       digitalWrite(ledPin, HIGH);
-      Serial.println("Mode: Right switch pressed");
+      Serial.println("Mode: right switch pressed");
       break;
   }
+  
+  // Control the RGB LED based on the current mode
+  // For a common cathode RGB LED: HIGH turns the LED on.
+  switch (currentMode) {
+    case MODE_LEFT:
+      // Red 
+      digitalWrite(rPin, HIGH);
+      digitalWrite(gPin, LOW);
+      digitalWrite(bPin, LOW);
+      break;
+    case MODE_NEITHER:
+      // yelow
+      digitalWrite(rPin, HIGH);
+      digitalWrite(gPin, HIGH);
+      digitalWrite(bPin, LOW);
+      break;
+    case MODE_RIGHT:
+      // green
+      digitalWrite(rPin, LOW);
+      digitalWrite(gPin, HIGH);
+      digitalWrite(bPin, LOW);
+      break;
+  }
+  
   delay(100); // Short delay for debounce and to avoid spamming the serial output
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// #define LED_PIN 8       // Onboard LED on ESP32-C3 SuperMini
-// #define SWITCH_PIN 3    // Connect your two-way switch here
-
-// void setup() {
-//   pinMode(LED_PIN, OUTPUT);           // Set the LED pin as output
-//   pinMode(SWITCH_PIN, INPUT_PULLUP);  // Set the switch pin as input with pull-up
-// }
-
-// void loop() {
-//   // Read the switch state (LOW when pressed due to pull-up)
-//   if (digitalRead(SWITCH_PIN) == LOW) {
-//     digitalWrite(LED_PIN, HIGH);  // Switch is pressed: turn LED on
-//   } else {
-//     digitalWrite(LED_PIN, LOW);   // Switch is not pressed: turn LED off
-//   }
-// }
